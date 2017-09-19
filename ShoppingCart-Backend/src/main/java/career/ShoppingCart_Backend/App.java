6f76b001c9d46a6bec333d922edb7b378857c3e3
Negative <sanketchavan>
2017-09-19 15:22:44 +0530

    This is for you

---
+++ ShoppingCart-Backend/src/main/java/career/ShoppingCart_Backend/App.java
@@ -0,0 +1,13 @@
+package career.ShoppingCart_Backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
